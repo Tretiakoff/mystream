197f8d1836c7ee8b183335490dc059bead01912c
Clara Fourcade Tretiakoff <c.tretiakoff@gmail.com>
2016-05-15 18:56:38 +0200

    Facebook Connect

--- src/EntityBundle/Entity/User.php
+++ src/EntityBundle/Entity/User.php
@@ -19,15 +19,18 @@ class User extends BaseUser
      *
      * @return int
      */
+
     public function getId()
     {
         return $this->id;
     }
 
+
     public function setEmail($email){
         parent::setEmail($email);
         $this->setUsername($email);
     }
+
     /**
      * Tells if the the given user is this user.
      *
@@ -39,6 +42,64 @@ class User extends BaseUser
      */
     public function isUser(UserInterface $user = null)
     {
-        
+
+    }
+    /**
+     * @var string
+     */
+    private $facebook_id;
+
+    /**
+     * @var string
+     */
+    private $facebook_access_token;
+
+
+    /**
+     * Set facebookId
+     *
+     * @param string $facebookId
+     *
+     * @return User
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebook_id = $facebookId;
+
+        return $this;
+    }
+
+    /**
+     * Get facebookId
+     *
+     * @return string
+     */
+    public function getFacebookId()
+    {
+        return $this->facebook_id;
+    }
+
+    /**
+     * Set facebookAccessToken
+     *
+     * @param string $facebookAccessToken
+     *
+     * @return User
+     */
+    public function setFacebookAccessToken($facebookAccessToken)
+    {
+        $this->facebook_access_token = $facebookAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get facebookAccessToken
+     *
+     * @return string
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebook_access_token;
     }
 }
