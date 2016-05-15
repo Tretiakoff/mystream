987f741672fbcd14d9588a7e378400b4b1690321
Clara Fourcade Tretiakoff <c.tretiakoff@gmail.com>
2016-05-15 13:15:33 +0200

    Fixed FOSUserBundle

--- src/EntityBundle/Entity/User.php
+++ src/EntityBundle/Entity/User.php
@@ -1,27 +1,40 @@
 <?php
-
 namespace EntityBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\UserInterface;
 
 /**
  * User
  */
-class User
+class User extends BaseUser
 {
     /**
      * @var int
      */
-    private $id;
-
-
+    protected $id;
     /**
      * Get id
      *
-     * @return integer 
+     * @return int
      */
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Tells if the the given user is this user.
+     *
+     * Useful when not hydrating all fields.
+     *
+     * @param null|UserInterface $user
+     *
+     * @return boolean
+     */
+    public function isUser(UserInterface $user = null)
+    {
+        // TODO: Implement isUser() method.
     }
 }
--- src/EntityBundle/EntityBundle.php
+++ src/EntityBundle/EntityBundle.php
@@ -2,7 +2,6 @@
 
 namespace EntityBundle;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EntityBundle extends Bundle
